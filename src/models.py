2d9608edb58b069c7398ceb98804b5a9cbb01e64
Django Doctor <help@django.doctor>
2021-01-13 10:11:33 +0000

    Fix some django anti-patterns

--- src/models.py
+++ src/models.py
@@ -3,29 +3,29 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50, null=True)
-    middle_name = models.CharField(max_length=50, null=True)
-    last_name = models.CharField(max_length=50, null=True)
-    email = models.EmailField(null=True)
-    phone = models.CharField(max_length=50, null=True)
-    address = models.CharField(max_length=300, null=True)
-    image = models.ImageField(null=True)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, blank=True)
+    first_name = models.CharField(max_length=50, default='', blank=True)
+    middle_name = models.CharField(max_length=50, default='', blank=True)
+    last_name = models.CharField(max_length=50, default='', blank=True)
+    email = models.EmailField(default='', blank=True)
+    phone = models.CharField(max_length=50, default='', blank=True)
+    address = models.CharField(max_length=300, default='', blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.last_name}, {self.first_name} {self.middle_name}'
 
     
 class Tag(models.Model):
-    name = models.CharField(max_length=100, null=True)
+    name = models.CharField(max_length=100, default='', blank=True)
 
     def __str__(self):
         return self.name
     
 
 class Product(models.Model):
-    name = models.CharField(max_length=150, null=True)
-    price = models.IntegerField(null=True)
+    name = models.CharField(max_length=150, default='', blank=True)
+    price = models.IntegerField(null=True, blank=True)
     date_created = models.DateField(auto_now_add=True)
     tags = models.ManyToManyField(Tag)
 
@@ -35,8 +35,8 @@ class Product(models.Model):
 class Order(models.Model):
     STATUS = (('Pending','Pending'), ('Out of Delivery','Out of Delivery'), ('Delivered', 'Delivered'))
 
-    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
-    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL, blank=True)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=100, null=True, choices=STATUS)
 
